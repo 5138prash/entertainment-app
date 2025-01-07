8a59fa40e5b385b5139b7b0c944111f0cd7f75a9
Prashant Tripathi <89456446+5138prash@users.noreply.github.com>
2025-01-07 17:31:53 +0530

    Update generateToken.js

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -4,15 +4,15 @@ const jwt = require("jsonwebtoken");  // Import the jsonwebtoken package for cre
 const generateToken = (res, userId) => {
     // Create a token with the user's ID, using the secret from the environment variables
     const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
-        expiresIn: "1d",  // The token will expire in 1 day
+        expiresIn: "30d",  // The token will expire in 1 day
     });
 
     // Set the JWT token in a cookie with the necessary configurations
     res.cookie("jwt", token, {
         httpOnly: true,  // Ensure the cookie is only accessible via HTTP requests (not accessible by JavaScript)
-        secure: process.env.NODE_ENV !== "development",  // Use secure cookies in production (HTTPS)
+        secure: process.env.NODE_ENV === "production",  // Use secure cookies in production (HTTPS)
         sameSite: "strict",  // Prevent cross-site request forgery (CSRF) attacks
-        maxAge: 24 * 60 * 60 * 1000, // Set the cookie's expiration time to 1 day in milliseconds
+        maxAge:30 * 24 * 60 * 60 * 1000,, // Set the cookie's expiration time to 1 day in milliseconds
     });
 };
 
